6603093e91a03acd477792bf6d1b435075b3cd89
Aleksandr Kartashov <aleksandrmon1@gmail.com>
2021-08-01 13:56:54 +0300

    add block bootstrap.html add username in profile

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -9,7 +9,7 @@ public class MainController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("title", "Главная страница");
+
         return "home";
     }
 
--- src/main/java/com/example/demo/controllers/ProfileController.java
+++ src/main/java/com/example/demo/controllers/ProfileController.java
@@ -1,16 +1,19 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class ProfileController {
 
     @GetMapping("/profile")
-    public String home(Model model) {
-        model.addAttribute("title", "Мой профиль");
+    public String home(@AuthenticationPrincipal User user, Model model) {
+        model.addAttribute("username",user.getUsername());
         return "profile";
     }
 
