7996eec5d8ac3da0ecb61fd6d0023d2ff3c984e7
Aleksandr Kartashov <aleksandrmon1@gmail.com>
2021-09-25 21:53:20 +0300

    add turbolinks

--- src/main/java/com/example/demo/config/MvcConfig.java
+++ src/main/java/com/example/demo/config/MvcConfig.java
@@ -1,9 +1,11 @@
 package com.example.demo.config;
 
 
+import com.example.demo.util.RedirectInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,5 +19,10 @@ public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry){
         registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RedirectInterceptor());
     }
 }
