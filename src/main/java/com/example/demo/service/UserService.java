e6c204c98d1376d4d3620accc7be55b15f227185
Aleksandr Kartashov <aleksandrmon1@gmail.com>
2021-09-29 13:20:42 +0300

    for heroku Mail link activate

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -128,7 +128,7 @@ public class UserService implements UserDetailsService {
 
         String message = String.format(
                 "Hello, %s \n" +
-                        "Welcome to blog. Please , visit next link: http://localhost:8080/activate/%s",
+                        "Welcome to blog. Please , visit next link: http://https://blog227.herokuapp.com/activate/%s",
                 user.getUsername(),
                 user.getActivationCode()
         );
